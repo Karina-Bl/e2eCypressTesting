5326dae7a1187d528e38f43dde81bd036df08a90
Karina B <blazeniakova.k@.com>
2023-11-21 10:05:56 +0100

    card numbers changed to strings

--- cypress/e2e/payPalSandbox/buyerCardsManipulation.cy.js
+++ cypress/e2e/payPalSandbox/buyerCardsManipulation.cy.js
@@ -1,23 +1,21 @@
-let username = Cypress.env("payPalBuyer");
-let password = Cypress.env("payPalbuyerPassword");
+const username = Cypress.env("payPalBuyer");
+const password = Cypress.env("payPalbuyerPassword");
 const cards = {
   Mastercard: {
     cardType: "master_card",
-    cardNo: 5428546769511978,
-    last4Digits: 1978,
+    cardNo: '5428546769511978',
+    last4Digits: '1978',
     expiryDate: "10/2026",
     CVC: "888",
   },
   Visa: {
     cardType: "visa",
-    cardNo: 4032036018546273,
-    last4Digits: 6273,
-    expiryDate: "06/2024",
-    CVC: "444",
+    cardNo: '4214024387634508',
+    last4Digits: '4508',
+    expiryDate: "01/2027",
+    CVC: "887",
   },
 };
-const wrongCardNo = 345679;
-
 describe("paypal buyer sandbox cards manipulation", () => {
   beforeEach(() => {
     cy.session("login", () => {
@@ -60,10 +58,10 @@ describe("paypal buyer sandbox cards manipulation", () => {
   it("fails to add the card with invalid number", () => {
     cy.visit("/myaccount/money/cards/new");
     cy.get(".test_achtodc_default > .col").click();
-    cy.get("#cardNumber").type(1234567);
+    cy.get("#cardNumber").type('1234567');
     cy.get("#brand").select("Mastercard");
     cy.get("#expDate").type("01/2011");
-    cy.get("#verificationCode").type(222);
+    cy.get("#verificationCode").type('222');
     cy.get('[data-testid="button-submit"]').click();
     cy.get('[role="alert"]');
   });
