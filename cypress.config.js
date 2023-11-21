0fe53860f7860442b899d5081384c72b3c7e8920
Karina B <blazeniakova.k@.com>
2023-11-21 09:49:04 +0100

    fixed eslint configuration

--- cypress.config.js
+++ cypress.config.js
@@ -3,13 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.sandbox.paypal.com/",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
     env: {
       payPalBuyer: "sb-k4rvc28158654@personal.example.com",
       payPalbuyerPassword: "8*sMSbKhP53Zf&mM",
     },
   },
-  env: {},
 });
